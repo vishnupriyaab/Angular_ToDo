b4ea007f51f3f11b90bde41c3c866f66cc050747
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-08-21 13:33:52 +0530

    task folder is added

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../../tasks';
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule,CommonModule],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent  implements OnInit{
   @Input()task!: Task;
+  faTimes = faTimes;
+  
 
   constructor(){}
 
