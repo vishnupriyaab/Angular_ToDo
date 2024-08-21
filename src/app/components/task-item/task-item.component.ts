6dd39d8f66239bbf41856bba423f061cfbcd139b
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-08-21 17:43:55 +0530

    add-task component is added

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,22 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../tasks';
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [FontAwesomeModule,CommonModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.css'
+  styleUrl: './task-item.component.css',
 })
-export class TaskItemComponent  implements OnInit{
-  @Input()task!: Task;
+export class TaskItemComponent implements OnInit {
+  @Input() task!: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
-  
 
-  constructor(){}
+  constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(task: any) {
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task: any) {
+    this.onToggleReminder.emit(task);
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,19 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { TASKS } from "../mock-tasks";
-import { Task } from "../../tasks";
+import { Task } from '../../tasks';
 import { CommonModule } from '@angular/common';
-import { TaskItemComponent } from "../task-item/task-item.component";
+import { TaskService } from '../../services/task.service';
+import { TaskItemComponent } from '../task-item/task-item.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   imports: [CommonModule, TaskItemComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent implements OnInit {
-  tasks:Task[] = TASKS;
-  constructor(){}
-  ngOnInit(): void {}
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter((t: Task) => t.id !== task.id);
+    });
+  }
+  toggleReminder(task:Task){
+    task.reminder = !task.reminder;
+     this.taskService.updateTaskReminder(task).subscribe();   
+  }
+  
 }
