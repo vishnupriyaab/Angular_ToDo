111211e511194ea746d16054d5b92e95600926de
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-08-21 19:15:05 +0530

    adding service-ui

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Task } from '../../tasks';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
-export class AddTaskComponent {
+// export class AddTaskComponent implements OnInit {
+//   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+//   text: string;
+//   day: string;
+//   reminder: boolean = false;
 
+//   constructor() {}
+//   ngOnInit(): void {}
+
+//   onSubmit() {
+//     if (!this.text) {
+//       alert('please add a task!');
+//       return;
+//     }
+
+//     const newTask = {
+//       text: this.text,
+//       day: this.day,
+//       reminder: this.reminder,
+//     };
+
+//     this.onAddTask.emit(newTask);
+//     this.text = '';
+//     this.day = '';
+//     this.reminder = false;
+//   }
+// }
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('please add a task!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -3,11 +3,12 @@ import { Task } from '../../tasks';
 import { CommonModule } from '@angular/common';
 import { TaskService } from '../../services/task.service';
 import { TaskItemComponent } from '../task-item/task-item.component';
+import { AddTaskComponent } from '../add-task/add-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, TaskItemComponent],
+  imports: [CommonModule, TaskItemComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
@@ -24,9 +25,12 @@ export class TasksComponent implements OnInit {
       this.tasks = this.tasks.filter((t: Task) => t.id !== task.id);
     });
   }
-  toggleReminder(task:Task){
+  toggleReminder(task: Task) {
     task.reminder = !task.reminder;
-     this.taskService.updateTaskReminder(task).subscribe();   
+    this.taskService.updateTaskReminder(task).subscribe();
   }
-  
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
+  }
 }
