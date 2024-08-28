70a1bbc0a89dac46f4df23d8345f6e2026bf89ab
vishnupriyaab <vishnupriyaotp2002@gmail.com>
2024-08-28 17:44:33 +0530

    small change in reverse tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -25,7 +25,7 @@ export class TasksComponent implements OnInit {
   }
   deleteTask(task: Task) {
     this.taskService.deleteTask(task).subscribe(() => {
-      this.tasks = this.tasks.filter((t: Task) => t.id !== task.id).reverse()
+      this.tasks = this.tasks.filter((t: Task) => t.id !== task.id)
     });
   }
   toggleReminder(task: Task) {
